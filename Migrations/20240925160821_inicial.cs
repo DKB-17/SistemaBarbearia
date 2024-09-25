4bd53154dd35e410ed1a04f98550c80ed9e9696e
diego <136401880+DKB-17@users.noreply.github.com>
2024-09-25 13:16:56 -0300

    Adicionar arquivos de projeto.

---
+++ Migrations/20240925160821_inicial.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SistemaBarbearia.Migrations
+{
+    /// <inheritdoc />
+    public partial class inicial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Estados",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    uf = table.Column<string>(type: "nvarchar(2)", maxLength: 2, nullable: false),
+                    nome = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Estados", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Estados");
+        }
+    }
+}
---
+++ Models/Contexto.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.DataProtection.KeyManagement;
+using Microsoft.EntityFrameworkCore;
+
+namespace SistemaBarbearia.Models
+{
+    public class Contexto : DbContext
+    {
+        public Contexto(DbContextOptions<Contexto> options) : base(options) { }
+
+        public DbSet<Estado> Estados { get; set; }
+    }
+}
