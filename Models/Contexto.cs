e3fe97efb421b3d793b99f366cf36cb39e02772e
Victor <victor.hugo.rf4@gmail.com>
2024-09-30 17:59:19 -0300

    Criação (Cliente, Horario e Servico) e alteração (Agenda, Barbeiro, Cidade da index) na pasta View

--- Models/Contexto.cs
+++ Models/Contexto.cs
@@ -12,5 +12,8 @@ namespace SistemaBarbearia.Models
         public DbSet<SistemaBarbearia.Models.Cidade> Cidade { get; set; }
         public DbSet<SistemaBarbearia.Models.Agenda> Agenda { get; set; }
         public DbSet<SistemaBarbearia.Models.Barbeiro> Barbeiro { get; set; }
+        public DbSet<SistemaBarbearia.Models.Cliente> Cliente { get; set; }
+        public DbSet<SistemaBarbearia.Models.Horario> Horario { get; set; }
+        public DbSet<SistemaBarbearia.Models.Servico> Servico { get; set; }
     }
 }
